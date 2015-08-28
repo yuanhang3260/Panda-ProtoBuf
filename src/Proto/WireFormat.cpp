d84d86164505e5e54bab49d26cdd31d7cc69146d
Hang Yuan <yuanhang3260@gmail.com>
2015-08-28 01:28:29 -0700

    primitive en/decode added

--- src/Proto/WireFormat.cpp
+++ src/Proto/WireFormat.cpp
@@ -1,3 +1,5 @@
+#include <exception>
+
 #include "WireFormat.h"
 
 namespace proto {
@@ -19,11 +21,45 @@ void WireFormat::WriteVariant64(uint64 value,
   }
 }
 
+void WireFormat::WriteFixed64(uint64 value,
+                              Utility::StringBuilder* outstream) {
+  outstream->Append(reinterpret_cast<const char*>(&value), sizeof(uint64));
+}
+
 void WireFormat::EncodeTag(uint32 tag, WireType wire_type,
                            Utility::StringBuilder* outstream) {
   WriteVariant32((tag << kWireTypeBits) | wire_type, outstream);
 }
 
+uint32 WireFormat::ZigZag32(int32 value) {
+  return (value << 1) ^ (value >> 31);
+}
+
+uint64 WireFormat::ZigZag64(int64 value) {
+  return (value << 1) ^ (value >> 63);
+}
+
+int32 WireFormat::UnZigZag32(uint32 value) {
+  return (value >> 1) ^ -static_cast<int32>(value & 1);
+}
+
+int64 WireFormat::UnZigZag64(uint64 value) {
+  return (value >> 1) ^ -static_cast<int64>(value & 1);
+}
+
+uint64 RawCastDoubleToUint64(double value) {
+  union { double f; uint64 i; };
+  f = value;
+  return i;
+}
+
+double RawCastUint64ToDouble(uint64 value) {
+  union { double f; uint64 i; };
+  i = value;
+  return f;
+}
+
+// -------------------- Decode functions ------------------------ //
 void WireFormat::EncodeUInt32(uint32 tag, uint32 value,
                               Utility::StringBuilder* outstream) {
   EncodeTag(tag, WIRETYPE_VARIANT, outstream);
@@ -35,5 +71,104 @@ void WireFormat::EncodeUInt64(uint32 tag, uint64 value,
   EncodeTag(tag, WIRETYPE_VARIANT, outstream);
   WriteVariant64(value, outstream);
 }
+
+void WireFormat::EncodeSInt32(uint32 tag, int32 value,
+                              Utility::StringBuilder* outstream) {
+  EncodeTag(tag, WIRETYPE_VARIANT, outstream);
+  WriteVariant32(ZigZag32(value), outstream);
+}
+
+void WireFormat::EncodeSInt64(uint32 tag, int64 value,
+                              Utility::StringBuilder* outstream) {
+  EncodeTag(tag, WIRETYPE_VARIANT, outstream);
+  WriteVariant64(ZigZag64(value), outstream);
+}
+
+void WireFormat::EncodeDouble(uint32 tag, double value,
+                              Utility::StringBuilder* outstream) {
+  EncodeTag(tag, WIRETYPE_FIXD64, outstream);
+  WriteFixed64(RawCastDoubleToUint64(value), outstream);
+}
+
+void WireFormat::EncodeBool(uint32 tag, bool value,
+                            Utility::StringBuilder* outstream) {
+  EncodeTag(tag, WIRETYPE_VARIANT, outstream);
+  WriteVariant64(static_cast<uint32>(value), outstream);
+}
+
+void WireFormat::EncodeString(uint32 tag, const std::string& str,
+                              Utility::StringBuilder* outstream) {
+  EncodeTag(tag, WIRETYPE_LENGTH_DIMITED, outstream);
+  WriteVariant32(str.length(), outstream);
+  for (uint32 i = 0; i < str.length(); i++) {
+    outstream->Append(str[i]);
+  }
+}
+
+// -------------------- Decode functions ------------------------ //
+uint32 WireFormat::DecodeUInt32(const char* buf, int* size) {
+  uint32 result = 0;
+  int index = 0, offset = 0, cnt = 0;
+  while (1) {
+    char c = buf[index++];
+    result |= ((c & kTagByteMask) << offset);
+    offset += kTagByteSize;
+    if ((c & (~kTagByteMask)) == 0) {
+      break;
+    }
+    cnt++;
+    if (cnt > 4) {
+      throw DecodeException("too long encoded unsigned 32");
+    }
+  }
+  *size = cnt + 1;
+  return result;
+}
+
+uint64 WireFormat::DecodeUInt64(const char* buf, int* size) {
+  uint64 result = 0;
+  int index = 0, offset = 0, cnt = 0;
+  while (1) {
+    char c = buf[index++];
+    result |= ((c & kTagByteMask) << offset);
+    offset += kTagByteSize;
+    if ((c & (~kTagByteMask)) == 0) {
+      break;
+    }
+    cnt++;
+    if (cnt > 9) {
+      throw DecodeException("too long encoded unsigned 64");
+    }
+  }
+  *size = cnt + 1;
+  return result;
+}
+
+int32 WireFormat::DecodeSInt32(const char* buf, int* size) {
+  uint32 raw_uint32 = DecodeUInt32(buf, size);
+  return UnZigZag32(raw_uint32);
+}
+
+int64 WireFormat::DecodeSInt64(const char* buf, int* size) {
+  uint64 raw_uint64 = DecodeUInt64(buf, size);
+  return UnZigZag64(raw_uint64);
+}
+
+double WireFormat::DecodeDouble(const char* buf, int* size) {
+  uint64 raw_uint64 = DecodeUInt64(buf, size);
+  return RawCastUint64ToDouble(raw_uint64);
+}
+
+bool WireFormat::DecodeBool(const char* buf, int* size) {
+  uint32 raw_uint32 = DecodeUInt32(buf, size);
+  return static_cast<bool>(raw_uint32);
+}
+
+std::string WireFormat::DecodeString(const char* buf, int size) {
+  return std::string(buf, size);
+}
+
+
+
 
 }  // namespace proto
--- src/Proto/WireFormat.h
+++ src/Proto/WireFormat.h
@@ -5,13 +5,26 @@
 
 namespace proto {
 
+typedef int int32;
+typedef long long int64;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+class DecodeException: public std::exception {
+ private:
+  std::string message_;
+ public:
+  explicit DecodeException(const std::string& message);
+  virtual const char* what() const throw() {
+      return message_.c_str();
+  }
+};
+
+DecodeException::DecodeException(const std::string& message) :
+  message_(message) {}
+
 class WireFormat {
  public:
-  typedef int int32;
-  typedef long long int64;
-  typedef unsigned int uint32;
-  typedef unsigned long long uint64;
-
   enum WireType {
     WIRETYPE_VARIANT            = 0,
     WIRETYPE_FIXD64             = 1,
@@ -33,10 +46,51 @@ class WireFormat {
   static void EncodeTag(uint32 tag, WireType wire_type,
                         Utility::StringBuilder* outstream);
 
+  static uint32 ZigZag32(int32 value);
+  static uint64 ZigZag64(int64 value);
+
+  static int32 UnZigZag32(uint32 value);
+  static int64 UnZigZag64(uint64 value);
+
+  static void WriteFixed64(uint64 value, Utility::StringBuilder* outstream);
+
+  static uint64 RawCastDoubleToUint64(double value);
+  static uint64 RawCastUint64ToDouble(uint64 value);
+
+
+  // ------------------- Encode functions ------------------------- //
   static void EncodeUInt32(uint32 tag, uint32 value,
                            Utility::StringBuilder* outstream);
   static void EncodeUInt64(uint32 tag, uint64 value,
                            Utility::StringBuilder* outstream);
+
+
+  static void EncodeSInt32(uint32 tag, int32 value,
+                           Utility::StringBuilder* outstream);
+  static void EncodeSInt64(uint32 tag, int64 value,
+                           Utility::StringBuilder* outstream);
+
+  static void EncodeDouble(uint32 tag, double value,
+                           Utility::StringBuilder* outstream);
+
+  static void EncodeBool(uint32 tag, bool value,
+                         Utility::StringBuilder* outstream);
+
+  static void EncodeString(uint32 tag, const std::string& str,
+                           Utility::StringBuilder* outstream);
+
+  // -------------------- Decode functions ------------------------ //
+  static uint32 DecodeUInt32(const char* buf, int* size);
+  static uint64 DecodeUInt64(const char* buf, int* size);
+
+  static int32 DecodeSInt32(const char* buf, int* size);
+  static int64 DecodeSInt64(const char* buf, int* size);
+
+  static double DecodeDouble(const char* buf, int* size);
+
+  static bool DecodeBool(const char* buf, int* size);
+
+  std::string DecodeString(const char* buf, int size);
 
  private:
   static void WriteVariant32(uint32 value, Utility::StringBuilder* outstream);
